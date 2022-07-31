91e3bacddcc4194697fff3978e27d0aa735f6668
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2022-07-31 22:09:06 +0200

    Add Main Menu script - change active screen and load specific level

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public List<GameObject> screens = new();
+
+    public void LoadLevel(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ActivateScreen(GameObject screenToActivate) {
+        foreach (var screen in screens) {
+            if (screen == screenToActivate) screen.SetActive(true);
+            else screen.SetActive(false);
+        }
+    }
+}
